b096b63ab29837e5c2891cc80e548246e9f1580d
Hubert Figuière <hub@collabora.com>
2025-11-20 16:27:00 -0500

    Added license to files
    Signed-off-by: Hubert Figuière <hub@collabora.com>

--- static/src/models/attachment_card_cool.js
+++ static/src/models/attachment_card_cool.js
@@ -1,8 +1,14 @@
 /** @odoo-module **/
-
-/**
+/* -*- js-indent-level: 4 -*- */
+/*
+ * Copyright the Collabora Online contributors.
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-
 
 import { AttachmentList } from "@mail/core/common/attachment_list";
 import { useService } from "@web/core/utils/hooks";

dd7d4bc8e97b1f8fd8cd2b13e55fedd7ee4c8905
Hubert Figuière <hub@collabora.com>
2025-12-04 17:15:57 -0500

    [odoo-17] NEWS file - Release 17.0.0.2.0
    Signed-off-by: Hubert Figuière <hub@collabora.com>

--- __manifest__.py
+++ __manifest__.py
@@ -8,7 +8,7 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 {
     'name': 'Collabora Online',
-    'version': '17.0.0.1.0',
+    'version': '17.0.0.2.0',
     'category': 'Productivity',
     'website': 'https://collaboraonline.com',
     'description': """

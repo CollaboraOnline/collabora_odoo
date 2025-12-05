6a1fbc8307977380c0145c9851ed8bc4eab2b17f
Hubert Figuière <hub@collabora.com>
2025-12-05 17:58:06 -0500

    [odoo-19] Port to Odoo 19
    - Update version
    - Show the attachments: DOM changed
    
    Signed-off-by: Hubert Figuière <hub@collabora.com>

--- __manifest__.py
+++ __manifest__.py
@@ -8,7 +8,7 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 {
     'name': 'Collabora Online',
-    'version': '18.0.0.2.0',
+    'version': '19.0.0.2.0',
     'category': 'Productivity',
     'website': 'https://collaboraonline.com',
     'description': """
